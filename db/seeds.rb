bf20524baad62af13ec80157ed7f9c537428c63b
Tomas K.T <tomas.konig.t@gmail.com>
2020-04-19 01:59:19 -0300

    Comentando el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-User.destroy_all
-Tweet.destroy_all
+# User.destroy_all
+# Tweet.destroy_all
 
-AdminUser.destroy_all
+# AdminUser.destroy_all
 
 10.times do
   User.create(
