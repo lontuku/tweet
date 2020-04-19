d5a0ca93181c2e6dfcb5f9702756bff0caca88a9
Tomas K.T <tomas.konig.t@gmail.com>
2020-04-19 02:35:31 -0300

    Comentando funcion de follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token, except: [:create, :update, :destroy]
-    def show 
-        tweet = Tweet.find(params[:id])
-        id = tweet.user.id
-        @user = User.find(id)
-        @relaciones = Follow.all
-        @follows = @user.follows
-        @followed = @follows == []
-        @followers = Follow.where(follow_id: id)
-    end
+    # def show 
+    #     tweet = Tweet.find(params[:id])
+    #     id = tweet.user.id
+    #     @user = User.find(id)
+    #     @relaciones = Follow.all
+    #     @follows = @user.follows
+    #     @followed = @follows == []
+    #     @followers = Follow.where(follow_id: id)
+    # end
 
     
     private
